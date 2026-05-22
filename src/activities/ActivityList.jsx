9a280be53026944ff6478b17cfa1617756dd0d61
Robert Bickley <robert.john.bickley@gmail.com>
2026-05-22 10:59:05 -0400

    Add ActivityList.jsx

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-import { createActivity, deleteActivity } from "../api/activities";
+import ActivityListItem from "./ActivityListItem";
 
-export default function ActivityList({ activities, token }) {
-  const [error, setError] = useState(null);
-
-
-
-
-
+export default function ActivityList({ activities }) {
   return (
     <ul>
       {activities.map((activity) => (
-      
+        <ActivityListItem key={activity.id} activity={activity} />
       ))}
     </ul>
   );
